fc40bc2151b4173118d1a5fc7a413f630a6708c9
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-05-09 13:37:44 +0900

    totalread.py: Cope with read=0 lines.

--- totalread.py
+++ totalread.py
@@ -3,8 +3,11 @@ from merge_readerror import *
 
 header, data = inputdata()
 totalread = 0
+totalvariation = 0
 for record in data:
     barcode, read, altered = record[:3]
     totalread += read
+    if read > 0:
+        totalvariation += 1
 print('variations', 'reads', sep='\t')
-print(len(data), totalread, sep='\t')
+print(totalvariation, totalread, sep='\t')

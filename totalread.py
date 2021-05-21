ab7914691bc7337a10066cca5cbc6dc62117a079
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-05-21 16:51:27 +0900

    totalread.py: Show mutation.

--- totalread.py
+++ totalread.py
@@ -9,12 +9,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     header, data = inputdata(has_header=(not args.noheader))
-    totalread = 0
+    totalread = totalmutation = 0
     totalvariation = 0
     for record in data:
-        barcode, read, altered = record[:3]
+        barcode, read, mutation = record[:3]
         totalread += read
+        totalmutation += mutation
         if read > 0:
             totalvariation += 1
-    print('variations', 'reads', sep='\t')
-    print(totalvariation, totalread, sep='\t')
+    print('variations', 'reads', 'mutation', sep='\t')
+    print(totalvariation, totalread, totalmutation, sep='\t')

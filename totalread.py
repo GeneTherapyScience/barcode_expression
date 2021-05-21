94017672e0daf2c8e92733719d3fc33087e012d4
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-05-21 16:05:04 +0900

    totalread.py: Add --noheader option.

--- totalread.py
+++ totalread.py
@@ -1,13 +1,20 @@
 #!/user/bin/python3
 from merge_readerror import *
+import argparse
 
-header, data = inputdata()
-totalread = 0
-totalvariation = 0
-for record in data:
-    barcode, read, altered = record[:3]
-    totalread += read
-    if read > 0:
-        totalvariation += 1
-print('variations', 'reads', sep='\t')
-print(totalvariation, totalread, sep='\t')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--noheader', action='store_true',
+                        help='the input does not include header.')
+    args = parser.parse_args()
+
+    header, data = inputdata(has_header=(not args.noheader))
+    totalread = 0
+    totalvariation = 0
+    for record in data:
+        barcode, read, altered = record[:3]
+        totalread += read
+        if read > 0:
+            totalvariation += 1
+    print('variations', 'reads', sep='\t')
+    print(totalvariation, totalread, sep='\t')

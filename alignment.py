33037d4ba1783825231f32c461831fe7ef7689f1
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-06-04 13:35:31 +0900

    alignment: Add option-parameter substitution=1 & let internal values be integer.

--- alignment.py
+++ alignment.py
@@ -11,11 +11,14 @@ for s in 'GCS':
     DNA_match_pairs |= {'S'+s, s+'S'}
 
 match_letters = 'MIXD'
-def seq_alignment(base, target, gap=2.5, extend=0.5, bound=None):
+def seq_alignment(base, target, gap=2.5, extend=0.5, substitution=1, bound=None):
     M, N = len(base), len(target)
+    gap = int(gap*100)
+    extend = int(extend*100)
+    substitution = int(substitution*100)
     if bound is None:
-        bound = (1+gap+extend)*(M+N+1)
-    cur = np.array([[i, bound, bound] for i in range(1,N+1)] + [(0,0,0)])
+        bound = (substitution+gap+extend)*(M+N+1)
+    cur = np.array([[i, bound, bound] for i in range(1,N+1)] + [(0,0,0)], dtype=int)
     # (normal, in-insertion, in-deletion)
     parent = np.zeros((M,N,3,3), dtype=int)
     for m in range(M):
@@ -23,7 +26,7 @@ def seq_alignment(base, target, gap=2.5, extend=0.5, bound=None):
         if k > bound:
             return k
         prev = cur
-        cur = np.zeros((N+1,3))
+        cur = np.zeros((N+1,3), dtype=int)
         cur[-1] = (m+1, bound, bound)
         for n in range(N):
             candidates = [
@@ -32,20 +35,20 @@ def seq_alignment(base, target, gap=2.5, extend=0.5, bound=None):
                     cur[n-1][0] + gap, # new insertion
                     cur[n-1][1] + extend, # continue insertion
                     cur[n-1][2] + gap, # new insetion
-                ]),
+                ], dtype=int),
                 np.array([
                     prev[n][0] + gap, # new deletion
                     prev[n][1] + gap, # continue deletion
                     prev[n][2] + extend, # new deletion
-                ]),
+                ], dtype=int),
             ]
             k = [candidates[i].argmin() for i in range(3)]
-            parent[m,n] = np.array([[m-1,n-1,k[0]], [m,n-1,k[1]], [m-1,n,k[2]]])
+            parent[m,n] = np.array([[m-1,n-1,k[0]], [m,n-1,k[1]], [m-1,n,k[2]]], dtype=int)
             cur[n] = [candidates[i][k[i]] for i in range(3)]
 
         m, n = M-1, N-1
         i = cur[N-1].argmin()
-        score = cur[N-1][i]
+        distance = cur[N-1][i]/substitution
         backarr = []
         while m >= 0:
             if i == 0:
@@ -61,7 +64,7 @@ def seq_alignment(base, target, gap=2.5, extend=0.5, bound=None):
             m, n, i = parent[m,n,i]
         matchseq = ''.join(backarr[::-1])
 
-    return matchseq, score
+    return matchseq, distance
 
 
 if __name__ == '__main__':

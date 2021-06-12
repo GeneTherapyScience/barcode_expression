df02c7778f2c0c6f600fdd8de4a0a3344cb48514
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-06-12 15:01:52 +0900

    Add stg2stgmixd.py

---
+++ stg2stgmixd.py
@@ -0,0 +1,12 @@
+from alignment import seq_alignment
+from merge_readerror import inputs
+from tqdm import trange
+
+default_reference = "GGTGGCTTTACCAACAGTAC"
+
+lines = [line.strip() for line in inputs()]
+N = len(lines)
+for i in trange(N):
+	stg = lines[i]
+	mixd, distance = seq_alignment(default_reference, stg)
+	print(stg, mixd, distance, sep='\t')

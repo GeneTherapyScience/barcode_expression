1194cb9a96956c8492d9f6a6fa0961c03a842922
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-06-14 21:25:01 +0900

    Add wsnstg2wsn.py

---
+++ codes/wsnstg2wsn.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import sys
+import os
+from collections import defaultdict
+import re
+from alignment import seq_alignment
+from merge_readerror import inputs
+
+usage = """\
+Usage : $ python wsnstg2wsn.py [--outdir=<outdir>] [<file1> <file2> ...]\
+"""
+
+stg_template = "GGTGGCTTTACCAACAGTAC"
+in_suffix = 'wsnstg'
+out_suffix = 'wsn'
+
+if __name__ == '__main__':
+    infile_namelist = sys.argv[1:]
+    diroption = "--outdir="
+    if infile_namelist and infile_namelist[0][:len(diroption)] == diroption:
+        outdir = infile_namelist[0][len(diroption):]
+        infile_namelist = infile_namelist[1:]
+    else:
+        outdir = "./"
+    if not infile_namelist:
+        infile_namelist.append(None)
+
+    for infile_name in infile_namelist:
+        if infile_name is None:
+            infile = sys.stdin
+        else:
+            infile = open(infile_name)
+
+        wsn_data = defaultdict(lambda: [0,0]) # read, var
+        header = infile.readline().rstrip('\n')
+        for line in inputs(infile):
+            wsn, stg, r = line.split()
+            r = int(r)
+            data = wsn_data[wsn]
+            data[0] += r
+            if stg != stg_template:
+                data[1] += r
+
+        if infile != sys.stdin:
+            infile.close()
+            outfile_name = os.path.join(outdir,
+                re.sub('\.'+in_suffix+'$', '', os.path.basename(infile_name)) + '.' + out_suffix
+                )
+            outfile = open(outfile_name, 'w')
+        else:
+            outfile = sys.stdout
+
+        print('WSN', 'Tolta', 'Mut', 'Mut_total', sep='\t', file=outfile)
+        for wsn, data in sorted(wsn_data.items(), key=lambda x: (-x[1][0], -x[1][1], x[0])):
+            r, mut = data
+            if r > 0:
+                mut_total = mut/r
+            else:
+                mut_total = 'N/A'
+            print(wsn, r, mut, mut_total, sep='\t', file=outfile)
+
+        if outfile != sys.stdout:
+            outfile.close()

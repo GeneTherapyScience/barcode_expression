db47b32bed8f9f3d6966c7dfad5f9c9972fb7a65
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-06-17 23:46:40 +0900

    totalread: Add --shannon option.

--- codes/totalread.py
+++ codes/totalread.py
@@ -2,6 +2,7 @@
 from merge_readerror import *
 from wsnstg2mutinfo import wsn_thres
 from bisect import bisect_left
+import math
 import argparse
 
 if __name__ == '__main__':
@@ -10,14 +11,16 @@ if __name__ == '__main__':
                         help='the input does not include header.')
     parser.add_argument('-r', '--ratiothres', type=float, default=0,
                         help='give ratio for threshold.')
+    parser.add_argument('-s', '--shannon', action='store_true',
+                        help='show Shannon-variation.')
     args = parser.parse_args()
 
     header, data = inputdata(has_header=(not args.noheader))
     totalread = totalmutation = 0
     totalvariation = 0
+    read_arr = []
     if args.ratiothres > 0:
         ratiothres_flag = True
-        read_arr = []
     else:
         ratiothres_flag = False
     for record in data:
@@ -26,8 +29,7 @@ if __name__ == '__main__':
         totalmutation += mutation
         if read > 0:
             totalvariation += 1
-        if ratiothres_flag:
-            read_arr.append(read)
+        read_arr.append(read)
     titles = ['variations', 'reads', 'mutation']
     values = [totalvariation, totalread, totalmutation]
     if ratiothres_flag:
@@ -36,5 +38,16 @@ if __name__ == '__main__':
         n = bisect_left(read_arr, th)
         titles.append('limited_variations')
         values.append(totalvariation - n)
+    if args.shannon:
+        S = -sum(
+            ((r/totalread) * math.log(r/totalread) if r>0 else 0)
+            for r in read_arr)
+        titles.append('Shannon-variation')
+        v = math.exp(S)
+        if v >= 100:
+            v = round(v)
+        else:
+            v = round(v,1)
+        values.append(v)
     print(*titles, sep='\t')
     print(*values, sep='\t')

266155008cfb4190959da6e664d718a2b7ecdb07
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-06-21 22:26:21 +0900

    checkWSN: Add --noheader option.

--- codes/checkWSN.py
+++ codes/checkWSN.py
@@ -2,20 +2,26 @@
 from merge_readerror import *
 from tqdm import tqdm, trange
 from collections import defaultdict
-# import argparse
+import argparse
 
-WSN = 'WSN'*10
-header, data = inputdata()
-N = len(data)
-variations = defaultdict(int)
-readnums = defaultdict(int)
-for i in trange(N):
-    barcode, readnum, altered = data[i]
-    d = levenshtein_distance(barcode, WSN, None)
-    variations[d] += 1
-    readnums[d] += readnum
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--noheader', action='store_true',
+                        help='the input does not include header.')
+    args = parser.parse_args()
 
-for dic, label in [(variations, 'Variation'), (readnums, 'Readnum')]:
-    dist = sorted([(d,n) for d, n in dic.items()])
-    av = sum(d*n for d,n in dist)/sum(n for d, n in dist)
-    print('{}-base: avarage = {}, distribution = {}'.format(label, av, dist))
+    WSN = 'WSN'*10
+    header, data = inputdata(has_header=(not args.noheader))
+    N = len(data)
+    variations = defaultdict(int)
+    readnums = defaultdict(int)
+    for i in trange(N):
+        barcode, readnum, altered = data[i]
+        d = levenshtein_distance(barcode, WSN, None)
+        variations[d] += 1
+        readnums[d] += readnum
+
+    for dic, label in [(variations, 'Variation'), (readnums, 'Readnum')]:
+        dist = sorted([(d,n) for d, n in dic.items()])
+        av = sum(d*n for d,n in dist)/sum(n for d, n in dist)
+        print('{}-base: avarage = {}, distribution = {}'.format(label, av, dist))

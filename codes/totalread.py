ea320fdedd6c460da165c35c7564b3943c2f390f
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-06-25 20:55:43 +0900

    totalread: Add --uniq, delete --shannon.

--- codes/totalread.py
+++ codes/totalread.py
@@ -11,37 +11,61 @@ if __name__ == '__main__':
                         help='the input does not include header.')
     parser.add_argument('-r', '--ratiothres', type=float, default=0,
                         help='give ratio for threshold.')
-    parser.add_argument('-s', '--shannon', action='store_true',
-                        help='show Shannon-variation.')
+    # parser.add_argument('-s', '--shannon', action='store_true',
+    #                     help='show Shannon-variation.')
+    parser.add_argument('--uniq', action='store_true',
+                        help='uniq file input.')
     args = parser.parse_args()
 
-    header, data = inputdata(has_header=(not args.noheader))
-    totalread = totalmutation = 0
-    totalvariation = 0
-    read_arr = []
-    if args.ratiothres > 0:
-        ratiothres_flag = True
+    if args.uniq:
+        totalread, totalvariation, S = 0, 0, 0
+        if not args.noheader:
+            header = input()
+        for line in inputs():
+            n = int(line.split()[0])
+            if n > 0:
+                totalvariation += 1
+                totalread += n
+                S -= math.log(n)*n
+        v = math.exp(S/totalread)*totalread
+        if v >= 100:
+            v = round(v)
+        else:
+            v = round(v,1)
+        titles = ['variations', 'reads', 'Shannon-variation']
+        values = [totalvariation, totalread, v]
+        print(*titles, sep='\t')
+        print(*values, sep='\t')
     else:
-        ratiothres_flag = False
-    for record in data:
-        barcode, read, mutation = record[:3]
-        totalread += read
-        totalmutation += mutation
-        if read > 0:
-            totalvariation += 1
-        read_arr.append(read)
-    titles = ['variations', 'reads', 'mutation']
-    values = [totalvariation, totalread, totalmutation]
-    if ratiothres_flag:
-        read_arr.sort()
-        th = wsn_thres(read_arr, ratio=args.ratiothres)
-        n = bisect_left(read_arr, th)
-        titles.append('limited_variations')
-        values.append(totalvariation - n)
-    if args.shannon:
+        header, data = inputdata(has_header=(not args.noheader))
+        totalread = totalmutation = 0
+        totalvariation = 0
+        read_arr = []
+        if args.ratiothres > 0:
+            ratiothres_flag = True
+        else:
+            ratiothres_flag = False
+        for record in data:
+            barcode, read, mutation = record[:3]
+            totalread += read
+            totalmutation += mutation
+            if read > 0:
+                totalvariation += 1
+            read_arr.append(read)
         S = -sum(
             ((r/totalread) * math.log(r/totalread) if r>0 else 0)
             for r in read_arr)
+
+        titles = ['variations', 'reads', 'mutation']
+        values = [totalvariation, totalread, totalmutation]
+        if ratiothres_flag:
+            read_arr.sort()
+            th = wsn_thres(read_arr, ratio=args.ratiothres)
+            n = bisect_left(read_arr, th)
+            titles.append('limited_variations')
+            values.append(totalvariation - n)
+
+        # shannon
         titles.append('Shannon-variation')
         v = math.exp(S)
         if v >= 100:
@@ -49,5 +73,5 @@ if __name__ == '__main__':
         else:
             v = round(v,1)
         values.append(v)
-    print(*titles, sep='\t')
-    print(*values, sep='\t')
+        print(*titles, sep='\t')
+        print(*values, sep='\t')

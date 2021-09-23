1e0c622daffb5dfe6acb597064215e7635f8ddb7
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2021-09-23 16:40:07 +0900

    mean_std: minor correction.

--- codes/mean_std.py
+++ codes/mean_std.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
 data = np.array([float(line) for line in sys.stdin.readlines()])
-print('mean:', data.mean(), sep='\t')
-print('std:', data.std(), sep='\t')
+print(
+    'mean = {}'.format(data.mean()),
+    'std = {}'.format(data.std()),
+    sep='\t')

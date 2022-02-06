a6ae58b52cf9549174c3ed075058ba3112a2bb73
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2022-02-07 00:28:41 +0900

    update stg_mut_stat.py

--- codes/stg_mut_stat.py
+++ codes/stg_mut_stat.py
@@ -6,6 +6,7 @@ import pandas as pd
 from collections import defaultdict
 # from matthijsz.weightedcorr.WeightedCorr import WeightedCorr
 from scipy import special
+import sys
 
 clones = ['N112', 'P112', 'S98']
 env = ['NT', 'DTX', 'Sp']
@@ -13,7 +14,7 @@ days = ['D0', 'D7', 'D14']
 E, T, K = 3, 3, 3
 target_t = 2
 t_range = [target_t]
-min_reads = 32
+min_reads = 4
 
 if __name__=='__main__':
     import argparse
@@ -24,31 +25,36 @@ if __name__=='__main__':
     for clone in clones:
         print(clone)
         barcodes = set()
-        reads = defaultdict(int)
         data = ad.get_celllines(clone, datadir='../wsnstg_white40_hamm2B/', common = '.merge.extracted.reformat.white40.filtered.wsnstg')
         for e, t, k in product(range(E), t_range, range(K)):
             barcodes |= set(data[e][t][k].keys())
         result = []
         for barcode in barcodes:
-            mu, V = np.empty((3,3)), np.empty((3,3))
+            mu, V, reads = [np.empty((3,3)) for _ in range(3)]
             for e, t, k in product(range(E), t_range, range(K)):
                 cur = data[e][t][k][barcode]
                 mu[e][k] = cur['insdel_mean']
                 V[e][k] = cur['insdel_V']
-                if cur['reads'] < min_reads:
-                    break
-            else:
-                max_e = np.argmax([mu[e][0] for e in range(E)])
-                other_e = set(range(E)) - {max_e}
-                if min(mu[max_e]) > max([mu[e][k] for e in other_e for k in range(K)]):
-                    w = min((mu[max_e][k0]-mu[oe][k1])/np.sqrt(V[max_e][k0]+V[oe][k1])
-                            for oe, k0, k1 in product(other_e, range(K), range(K)))
-                    result.append((1-special.erf(w), max_e, barcode))
+                reads[e][k] = cur['reads']
+            for max_e in range(E):
+                # other_e = set(range(E)) - {max_e}
+                if min(reads[max_e].sum(),reads.sum()-reads[max_e].sum()) < min_reads:
+                    continue
+                m0 = (mu[max_e] @ reads[max_e]) / reads[max_e].sum()
+                m1 = (np.tensordot(mu,reads,((0,1),(0,1))) - mu[max_e] @ reads[max_e]) / (reads.sum() - reads[max_e].sum())
+                V0 = (V[max_e] + mu[max_e]**2) @ reads[max_e] / reads[max_e].sum() - m0*m0
+                V1 = (np.tensordot(V+mu**2, reads, ((0,1),(0,1))) - (V[max_e]+mu[max_e]**2) @ reads[max_e]) / (reads.sum() - reads[max_e].sum()) - m1*m1
+                if min(V0,V1) <= 0:
+                    print(V0, V1, file=sys.stderr)
+                sigma = np.sqrt(V0 + V1)
+                if m0 <= m1:
+                    continue
+                w = (m0 - m1)/sigma
+                result.append((1-special.erf(w), max_e, barcode, reads.sum()))
         result.sort()
         N = 32
-        print('{} barcodes, {} are 3-6 structure & all more than {} reads.'.format(len(barcodes),len(result),min_reads))
         n = min(N, len(result))
         print('Most significant {} are:'.format(n))
-        print('p-value', 'env', 'barcode', sep='\t')
+        print('p-value', 'env', 'barcode', 'reads', sep='\t')
         for i in range(n):
-            print(result[i][0], env[result[i][1]], result[i][2], sep='\t', flush=True)
+            print(result[i][0], env[result[i][1]], result[i][2], result[i][3], sep='\t', flush=True)

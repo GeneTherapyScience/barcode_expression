277263ae06accc5624165a2b838a79f13eb77d9c
OGAWA Noriaki <u429576f@ecs.osaka-u.ac.jp>
2022-02-13 19:53:01 +0900

    Delete utils/mean.std.py

--- codes/utils/mean_std.py
+++
@@ -1,7 +0,0 @@
-import sys
-import numpy as np
-data = np.array([float(line) for line in sys.stdin.readlines()])
-print(
-    'mean = {}'.format(data.mean()),
-    'std = {}'.format(data.std()),
-    sep='\t')
